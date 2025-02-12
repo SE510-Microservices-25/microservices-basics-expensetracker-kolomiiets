4499b3f7fe3e96edba4f615f24762ffb7c362304
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2025-02-12 13:35:44 +0200

    Add Swagger and update Program.cs

--- ExpenseService/obj/Debug/net8.0/ExpenseService.AssemblyInfo.cs
+++ ExpenseService/obj/Debug/net8.0/ExpenseService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+388db1cd14ea04debe2b1e5d23e19b1442e358b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed05d93735acd09cc7efd4ae01b19f8e5daaf5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
