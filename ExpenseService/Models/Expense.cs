388db1cd14ea04debe2b1e5d23e19b1442e358b0
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2025-02-12 13:30:08 +0200

    Add expense model

---
+++ ExpenseService/Models/Expense.cs
@@ -0,0 +1,10 @@
+namespace ExpenseService.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
